84183646f6b6b237b856fb1dd05a1c8ab31a3f6f
kalibetre <kalbetre@icloud.com>
2023-05-21 16:51:52 +0300

    additional tests

--- 0x08-making_change/0-main.py
+++ 0x08-making_change/0-main.py
@@ -3,8 +3,10 @@
 Main file for testing
 """
 
-makeChange = __import__('0-making_change').makeChange
+makeChange = __import__('0-making_change_rec').makeChange
 
+print(makeChange([1, 4, 5], 8))
 print(makeChange([1, 2, 25], 37))
-
+print(makeChange([25, 10, 5, 2, 1], 2147483647))
 print(makeChange([1256, 54, 48, 16, 102], 1453))
+print(makeChange([4, 5], 7))
