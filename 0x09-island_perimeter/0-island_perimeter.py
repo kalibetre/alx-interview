cc19f8b63c9fe960535479caac32aa7b3fa65e6b
kalibetre <kalbetre@icloud.com>
2023-05-27 13:06:33 +0300

    assume the gird could be an island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -15,10 +15,10 @@ def island_perimeter(grid):
     next_cells = []
     visited_cells = []
 
-    for i in range(1, height - 2):
+    for i in range(0, height):
         if (len(next_cells)):
             break
-        for j in range(1, width - 2):
+        for j in range(0, width):
             if (grid[i][j] == 0):
                 continue
             else:
@@ -30,30 +30,32 @@ def island_perimeter(grid):
 
     while (len(next_cells) > 0):
         cell = next_cells.pop()
+        if cell in visited_cells:
+            continue
 
         left = (cell[0], cell[1] - 1)
         top = (cell[0] - 1, cell[1])
         right = (cell[0], cell[1] + 1)
         bottom = (cell[0] + 1, cell[1])
 
-        if (grid[left[0]][left[1]] == 0):
+        if (left[1] < 0 or grid[left[0]][left[1]] == 0):
             perimeter += 1
-        elif (left not in visited_cells):
+        else:
             next_cells.append(left)
 
-        if (grid[top[0]][top[1]] == 0):
+        if (top[0] < 0 or grid[top[0]][top[1]] == 0):
             perimeter += 1
-        elif (top not in visited_cells):
+        else:
             next_cells.append(top)
 
-        if (grid[right[0]][right[1]] == 0):
+        if (right[1] >= width or grid[right[0]][right[1]] == 0):
             perimeter += 1
-        elif (right not in visited_cells):
+        else:
             next_cells.append(right)
 
-        if (grid[bottom[0]][bottom[1]] == 0):
+        if (bottom[0] >= height or grid[bottom[0]][bottom[1]] == 0):
             perimeter += 1
-        elif (bottom not in visited_cells):
+        else:
             next_cells.append(bottom)
 
         visited_cells.append(cell)
