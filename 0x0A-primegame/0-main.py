58ca28fc1023cbda69d3b2c209e7ce4c485daaec
kalibetre <kalbetre@icloud.com>
2023-06-01 18:00:44 +0300

    add more tests

--- 0x0A-primegame/0-main.py
+++ 0x0A-primegame/0-main.py
@@ -4,5 +4,7 @@ Main file for testing
 """
 isWinner = __import__('0-prime_game').isWinner
 
+print("Winner: {}".format(isWinner(0, [])))
+print("Winner: {}".format(isWinner(1, [10000, 1])))
 print("Winner: {}".format(isWinner(3, [4, 5, 1])))
 print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
